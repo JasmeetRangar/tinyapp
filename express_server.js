dd9e071a3862a3dbe34a1bcfd4a1c2e985aa7f83
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-02-15 18:16:36 +0000

    added different responses to server for diffent paths

--- express_server.js
+++ express_server.js
@@ -6,12 +6,17 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//response for homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+//response for /urls.json
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+//response for /hello
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
