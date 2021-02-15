0f77ff821cea0e435aebe78f9d02968927ea4d3a
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-02-15 20:03:48 +0000

    added form response to object method and generate random string function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,18 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+function generateRandomString() {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < 6; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -32,6 +43,10 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
